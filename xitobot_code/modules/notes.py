207feebd9dd30e65a78977ce12780920dc4dc7bb
Alexxito92 <alexpitzalis1992@gmail.com>
2023-10-20 17:44:20 +0200

    Implemented a basic version of notes, still have to code the get_note which is going to be pretty hard

--- xitobot_code/modules/notes.py
+++ xitobot_code/modules/notes.py
@@ -2,18 +2,48 @@ from telegram import Update
 from telegram.ext import CommandHandler, ContextTypes
 from telegram.constants import ParseMode
 from xitobot_code import application
-from xitobot_code.modules.tool_modules.get_type import get_message_type
-from xitobot_code.modules.db_modules.notes_db import add_note_to_db
+from xitobot_code.modules.tool_modules.get_type import get_note_type
+from xitobot_code.modules.db_modules.notes_db import add_note_to_db, list_all, clear_all_notes
 
-async def save(update: Update, context: ContextTypes):
+async def save(update: Update, context: ContextTypes.DEFAULT_TYPE):
     chat_id = update.effective_chat.id
-    msg = update.effective_message
 
-    note_name, text, data_type, content = get_message_type(msg)
-    await add_note_to_db(chat_id, note_name, text, data_type, content)
+    note_name, text, data_type, content = get_note_type(update.effective_message)
 
-async def get_all_notes(update: Update, context: ContextTypes):
-    pass
+    if note_name == None:
+        await update.effective_message.reply_text("Please insert a nome for the note.")
+    elif not update.effective_message.reply_to_message and data_type == None:
+        await update.effective_message.reply_text("Please provide some content for the note.")
+    else:
+        await update.effective_message.reply_text(f"Saved {note_name}.")
+        await add_note_to_db(chat_id, note_name, text, data_type, content)
+
+
+async def get_all_notes(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    chat_id = update.effective_chat.id
+    chat_name = update.effective_chat.effective_name
+    notes_list = list_all(chat_id)
+    if notes_list:
+        filtered_list = "\n".join([str("- "+document["name"]) for document in notes_list])
+        final_list = f"List of all notes in {chat_name}:\n\- "+filtered_list
+    else:
+        final_list = f"I didn't find any note to show in {chat_name}\."
+    await context.bot.send_message(chat_id, final_list, parse_mode=ParseMode.MARKDOWN_V2)
+
+async def del_all_notes(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    chat_id = update.effective_chat.id
+    chat_name = update.effective_chat.effective_name
+    deleted = clear_all_notes(chat_id)
+    if deleted == -1:
+        await update.effective_message.reply_text(f"There aren't any notes to delete in {chat_name}\.", parse_mode=ParseMode.MARKDOWN_V2)
+    else:
+        await update.effective_message.reply_text(f"Deleted {deleted} notes from {chat_name}\.", parse_mode=ParseMode.MARKDOWN_V2)
+
 
 save_handler = CommandHandler("save", save)
+get_all_notes_handler = CommandHandler("notes", get_all_notes)
+del_all_notes_handler = CommandHandler("clearnotes", del_all_notes)
+
 application.add_handler(save_handler)
+application.add_handler(get_all_notes_handler)
+application.add_handler(del_all_notes_handler)
--- xitobot_code/modules/tool_modules/get_type.py
+++ xitobot_code/modules/tool_modules/get_type.py
@@ -1,5 +1,5 @@
 from enum import IntEnum
-from telegram import Message
+from telegram import Update, Message
 
 class Types(IntEnum):
     TEXT = 0
@@ -10,39 +10,68 @@ class Types(IntEnum):
     VOICE = 5
     VIDEO = 6
 
-def get_message_type(msg: Message):
+def get_note_type(note_msg):
     data_type = None
     content = None
     text = ""
-    raw_text = msg.caption or msg.text
+    raw_text = note_msg.text
     args = raw_text.split(maxsplit=2)
-    note_name = args[1]
-
-    if len(args) >= 3:
-        data_type = Types.TEXT
-        text = args[2]
-    else:        
-        if msg.reply_to_message.text:
+    reply = note_msg.reply_to_message
+    
+    if len(args) == 1:
+        note_name = None
+    else:
+        note_name = args[1]
+        if reply and len(args) >= 3:
+            if reply.text:
+                data_type = Types.TEXT
+                text = str(reply.text).join(args[2])
+            elif reply.sticker:
+                data_type = Types.STICKER
+                content = reply.sticker.file_id
+            elif reply.document:
+                data_type = Types.DOCUMENT
+                content = reply.document.file_id
+                text = str(reply.caption).join(args[2])
+            elif reply.photo:
+                data_type = Types.PHOTO
+                content = reply.photo[-1].file_id
+                text = str(reply.caption).join(args[2])
+            elif reply.audio:
+                data_type = Types.AUDIO
+                content = reply.audio.file_id
+                text = str(reply.caption).join(args[2])
+            elif reply.video:
+                data_type = Types.VIDEO
+                content = reply.video.file_id
+                text = str(reply.caption).join(args[2])
+        elif not reply and len(args)>=3:
             data_type = Types.TEXT
-            text = msg.reply_to_message.text
-        elif msg.reply_to_message.sticker:
-            data_type = Types.STICKER
-            content = msg.reply_to_message.sticker.file_id
-        elif msg.reply_to_message.document:
-            data_type = Types.DOCUMENT
-            content = msg.reply_to_message.document.file_id
-            text = msg.reply_to_message.caption
-        elif msg.reply_to_message.photo:
-            data_type = Types.PHOTO
-            content = msg.reply_to_message.photo[-1].file_id
-            text = msg.reply_to_message.caption
-        elif msg.reply_to_message.audio:
-            data_type = Types.AUDIO
-            content = msg.reply_to_message.audio.file_id
-            text = msg.reply_to_message.caption
-        elif msg.reply_to_message.video:
-            data_type = Types.VIDEO
-            content = msg.reply_to_message.video.file_id
-            text = msg.reply_to_message.caption
+            text = args[2]
+        elif reply and len(args)==2:
+            if reply.text:
+                data_type = Types.TEXT
+                text = reply.text
+            elif reply.sticker:
+                data_type = Types.STICKER
+                content = reply.sticker.file_id
+            elif reply.document:
+                data_type = Types.DOCUMENT
+                content = reply.document.file_id
+                text = reply.caption
+            elif reply.photo:
+                data_type = Types.PHOTO
+                content = reply.photo[-1].file_id
+                text = reply.caption
+            elif reply.audio:
+                data_type = Types.AUDIO
+                content = reply.audio.file_id
+                text = reply.caption
+            elif reply.video:
+                data_type = Types.VIDEO
+                content = reply.video.file_id
+                text = reply.caption
+        else:
+            text = ""
 
     return note_name, text, data_type, content 
